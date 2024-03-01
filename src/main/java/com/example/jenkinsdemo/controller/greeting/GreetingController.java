83bd6912620d0fa1a43cea45b6da5920fd3ff275
Thomas <thomaspeters15@hotmail.com>
2024-03-01 15:54:37 +0100

    structure improve

--- src/main/java/com/example/jenkinsdemo/controller/greeting/GreetingController.java
+++ src/main/java/com/example/jenkinsdemo/controller/greeting/GreetingController.java
@@ -1,11 +1,11 @@
-package com.example.jenkinsdemo.controller;
+package com.example.jenkinsdemo.controller.greeting;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/greeting")
 public class GreetingController
 {
     @GetMapping
