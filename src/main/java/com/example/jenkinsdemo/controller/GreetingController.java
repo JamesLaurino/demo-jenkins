cb42e15ac605ac4f3e1a6768548ae302f9b5c10d
Thomas <thomaspeters15@hotmail.com>
2024-03-01 15:48:00 +0100

    First commit

---
+++ src/main/java/com/example/jenkinsdemo/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GreetingController
+{
+    @GetMapping
+    public String greeting()
+    {
+        return "Hello there !";
+    }
+}
